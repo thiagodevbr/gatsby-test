80e07d1a609f680df38274dc6bbc35ecc91214b6
Thiago Brito <britotr@gmail.com>
2024-10-10 20:16:54 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
